152e7e2e00d4d924cb311f7ad85b4fb7c0a3445c
bloycey <bloyce1990@gmail.com>
2017-02-23 21:04:56 +1000

    Created an initialisation function
    Only element outside IIFE

--- app.js
+++ app.js
@@ -41,15 +41,25 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
-    
+
+var setupEventListeners = function() {
+        
     var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event) {
+    
+    if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+    } 
+}); 
+};
     
     var ctrlAddItem = function() {
      
         // 1. Get the field input data;
         
         var input = UICtrl.getInput();
-        console.log(input);
      
      // 2. Add the item to the budget controller;
      
@@ -59,21 +69,18 @@ var controller = (function(budgetCtrl, UICtrl) {
      
      // 5. Display the budget on the UI; 
      
-    }
+    };
     
-document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
     
-    
-document.addEventListener('keypress', function(event) {
-    
-    if (event.keyCode === 13 || event.which === 13) {
-        ctrlAddItem();
-    }
   
-    
-});   
     
 })(budgetController, UIController);
 
 
-
+controller.init();
